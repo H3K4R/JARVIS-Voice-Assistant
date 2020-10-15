291de293b4f6bb56dadadcf20298be2cf5cd3238
Swarnava Basu <basuswarnava2@gmail.com>
2020-10-15 21:44:46 +0530

    Working on

--- jarvis.py
+++ jarvis.py
@@ -1,9 +1,9 @@
 import pyttsx3
-
+import datetime
 
 engine = pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
-print(voices[1].id)
+#print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
 
 
@@ -11,5 +11,18 @@ def speak(audio):
 	engine.say(audio)
 	engine.runAndWait()
 
+
+def wishMe():
+	hour = int(datetime.datetime.now().hour)
+	if hour>=0 and hour<12:
+		speak("Good Morning!!")
+	elif hour>=12 and hour<18:
+		speak("Good Afternoon!!")
+	else:
+		speak("Good Evening!!")
+
+	speak("I am JARVIS Sir. Please tell me how can I help you?")
+
+
 if __name__ == "__main__":
-	speak("Hello Barun Kumar Basu, How are you ? How your business going on?")
+	wishMe()
